e39ab7b09b01356797a141d0cb054ea953f66539
John Pettigrew <john.pettigrew404@gmail.com>
2016-09-17 22:08:50 -0500

    Add message lib

---
+++ message/message.go
@@ -0,0 +1,82 @@
+package message
+
+import (
+	"errors"
+	"strings"
+)
+
+type Message struct {
+	Command string
+	Options []string
+}
+
+var aliases = map[string]string{}
+
+func Marshal(m Message) string {
+
+	fullCmd := []string{}
+
+	//get correct command
+	cmd := strings.ToUpper(m.Command)
+	if aliases[cmd] != "" {
+		cmd = aliases[cmd]
+	}
+
+	//check for multi-word last option
+	finalOption := ""
+	if len(strings.Split(m.Options[len(m.Options)-1], " ")) > 1 {
+		finalOption = ":" + m.Options[len(m.Options)-1]
+		m.Options = m.Options[:len(m.Options)-1]
+	}
+
+	options := strings.Join(m.Options, " ")
+	fullCmd = append(fullCmd, cmd)
+	if options != "" {
+		fullCmd = append(fullCmd, options)
+	}
+	if finalOption != "" {
+		fullCmd = append(fullCmd, finalOption)
+	}
+
+	return strings.Join(fullCmd, " ")
+}
+
+func Unmarshal(input string) (Message, error) {
+
+	msg := Message{}
+
+	if input == "" {
+		return Message{}, errors.New("Input cannot be empty")
+	}
+
+	pieces := strings.Split(input, " ")
+	if len(pieces) < 2 {
+		return Message{}, errors.New("A command is required")
+	}
+
+	//Check for prefix
+	if string(pieces[0][0]) == ":" {
+		//remove extra data
+		pieces = pieces[1:]
+	}
+
+	//get command
+	msg.Command = pieces[0]
+	if len(pieces) == 1 {
+		return msg, nil
+	}
+	pieces = pieces[1:]
+
+	//get options
+	for pieceIndex, piece := range pieces {
+		//Get any multi-word last argument
+		if string(piece[0]) == ":" {
+			pieces[pieceIndex] = string(piece[1:])
+			msg.Options = append(msg.Options, strings.Join(pieces[pieceIndex:], " "))
+			break
+		}
+		msg.Options = append(msg.Options, piece)
+	}
+
+	return msg, nil
+}
---
+++ message/parser_test.go
@@ -0,0 +1,69 @@
+package message
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMarshal(t *testing.T) {
+	type testCase struct {
+		m      Message
+		result string
+	}
+	cases := []testCase{
+		testCase{
+			Message{Command: "join", Options: []string{"#golang"}},
+			"JOIN #golang",
+		},
+		testCase{
+			Message{Command: "ping", Options: []string{"chat.freenode.net"}},
+			"PING chat.freenode.net",
+		},
+		testCase{
+			Message{Command: "user", Options: []string{"name", "name", "irc.freenode.net", "Some Cool Name"}},
+			"USER name name irc.freenode.net :Some Cool Name",
+		},
+	}
+
+	for _, c := range cases {
+		result := Marshal(c.m)
+		assert.Equal(t, c.result, result)
+	}
+}
+
+func TestUnmarshal(t *testing.T) {
+	type testCase struct {
+		input string
+		m     Message
+		err   error
+	}
+
+	cases := []testCase{
+		testCase{
+			"JOIN #golang",
+			Message{Command: "JOIN", Options: []string{"#golang"}},
+			nil,
+		},
+		testCase{
+			"PING chat.freenode.net",
+			Message{Command: "PING", Options: []string{"chat.freenode.net"}},
+			nil,
+		},
+		testCase{
+			"USER name name irc.freenode.net :Some Cool Name",
+			Message{Command: "USER", Options: []string{"name", "name", "irc.freenode.net", "Some Cool Name"}},
+			nil,
+		},
+		testCase{
+			":aaa!bbb@ccc/ddd PRIVMSG #golang :Some cool message!",
+			Message{Command: "PRIVMSG", Options: []string{"#golang", "Some cool message!"}},
+			nil,
+		},
+	}
+
+	for _, c := range cases {
+		m, err := Unmarshal(c.input)
+		assert.Nil(t, c.err, err)
+		assert.Equal(t, c.m, m)
+	}
+}
